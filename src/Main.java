61abfc93146f7e41259aa8c0b926efc0d6310bd4
JAAR <joe.aaron.acosta@outlook.com>
2024-10-15 21:20:08 -0700

    Format for the main method and overall program functionality.

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+/* Programmer: JAAR
+ *  Assignment Chapter 5: Question 22
+ *  Date modified: 10/15/2024
+ *  IDE/Tool used: IntelliJ
+ */
+
+// Lets the user play a dice game and tallies up how many times the user wins.
+
+public class Main {
+    public static void main(String[] args) {
+        // Starts the dice game
+        // Plays each game with a delay between them
+        // Updates teh scoreboard
+        // Shows the final tally
+        // Congratulates the user if they won this round of games
+        // Otherwise tells them better luck next time.
+        // Saves the games as a binary file. for each individual game.
+    }
+}
